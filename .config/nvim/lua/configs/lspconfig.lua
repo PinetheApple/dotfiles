267eb5acf670bb542354c724ce500185df97607e
PinetheApple <pinespace889@gmail.com>
2024-10-24 16:33:20 +0530

    mostly nvim configuration

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,66 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
--- EXAMPLE
-local servers = { "html", "cssls", "bashls", "ast_grep", "lua_ls", "powershell_es", "rust_analyzer" }
-local nvlsp = require "nvchad.configs.lspconfig"
+-- list of all servers configured.
+lspconfig.servers = {
+    "lua_ls",
+    "clangd",
+    "powershell_es",
+    "pyright",
+    "rust_analyzer",
+}
+
+-- list of servers configured with default config.
+local default_servers = { "html", "cssls", "bashls", "pyright", "ast_grep" }
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+for _, lsp in ipairs(default_servers) do
+    lspconfig[lsp].setup({
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    })
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false, -- Disable all diagnostics from lua_ls
+                globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                    "${3rd}/love2d/library",
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
+
+-- lspconfig.pyright.setup({
+--     on_attach = on_attach,
+--     on_init = on_init,
+--     capabilities = capabilities,
+--
+--     settings = {
+--         python = {
+--             analysis = {
+--                 typeCheckingMode = "off", -- Disable type checking diagnostics
+--             },
+--         },
+--     },
+-- })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,7 @@
-require "nvchad.options"
+require("nvchad.options")
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = false
