f5ae83073e9b89db4c187e38648f55000d5abd95
PinetheApple <pinespace889@gmail.com>
2024-10-30 15:40:07 +0530

    updated script

--- .config/hypr/hyprpaper.sh
+++ .config/hypr/hyprpaper.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-load_wallpaper() {
+load_wallpapers() {
 	local wallpaper_dir="$HOME/Pictures/Wallpapers"
+	local first_image_found=false
+	local wallpaper=""
+
+	while IFS= read -r image; do
+        if [ "$first_image_found" = false ]; then
+            wallpaper="$image"
+            first_image_found=true
+        fi
+        echo "preload = $image" >> "$config_file"
+    done < <(find "$wallpaper_dir" -type f \( -iname "*.jpg" -o -iname "*.png" \))
 	
-	find "$wallpaper_dir" -type f \( \
-		-iname "*.jpg" -o \
-		-iname "*.png" \
-	\) | sed 's/^/preload = /' >> "$config_file"	
+	echo -e "\nwallpaper = ,$wallpaper" >> "$config_file"  # set current wallpaper
 }
 
 config_file="$HOME/.config/hypr/hyprpaper2.conf"
-> "$config_file"
-load_wallpaper
+> "$config_file"  # clear config file
+
+load_wallpapers
+
+echo -e "\nsplash = false" >> "$config_file"
