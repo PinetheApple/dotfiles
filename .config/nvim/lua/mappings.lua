512529231a35187c595286fa31ee81412747ee3b
PinetheApple <pinespace889@gmail.com>
2024-11-07 15:36:43 +0530

    added keybinds for commenting and terminal

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,19 @@
-require "nvchad.mappings"
-
--- add yours here
+require("nvchad.mappings")
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- terminal
+map({ "n", "t", "i", "v" }, "<C-`>", function()
+    require("nvchad.term").toggle({ pos = "sp", id = "htoggleTerm" })
+end, { desc = "toggle horizontal terminal" })
+
+-- comments
+map("n", "<C-/>", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<C-/>", "gc", { desc = "toggle comment", remap = true })
+
+-- tabufline
+map("n", "<C-x>", function()
+    require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
