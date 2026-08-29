aedc5b4719d47a6c581a4307667417f4c867d03b
PinetheApple <75367614+PinetheApple@users.noreply.github.com>
2026-08-29 19:43:00 +0530

    chore(waybar): remove unused waybar config
    Bar duties moved to wayle; the waybar files were no longer loaded.

--- .config/waybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# quit all running waybar instances
-# to reload waybar properly
-killall waybar
-
-# load config and style file while launching waybar
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
